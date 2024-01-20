4a6fbea6d7d4716a6a539bcb31750bbea6d75e85
tharanga270 <developerwordpress270@gmail.com>
2024-01-20 14:09:20 +0530

    5.login complete

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,8 +7,20 @@ import axios from "axios";
 const Login = () => {
   const navigate = useNavigate();
   //form handler
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("something went wrong");
+    }
   };
   return (
     <div className="form-container ">
