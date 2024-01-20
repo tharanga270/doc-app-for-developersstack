1d68e10f1965b71ddc325d930e35604208928b41
tharanga270 <developerwordpress270@gmail.com>
2024-01-20 15:33:46 +0530

    6.Authorization with JWT

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {
   loginController,
   registerController,
+  authController,
 } = require('../controllers/userCtrl');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 //router object
 const router = express.Router();
@@ -13,4 +15,11 @@ router.post('/login', loginController);
 
 //REGISTER || POST
 router.post('/register', registerController);
+
+//Auth || POST
+router.post('/getUserData', authMiddleware, authController);
+
 module.exports = router;
+
+
+
