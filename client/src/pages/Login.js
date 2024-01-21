93b7dc9fcf808b70d1e57769ee727cbb0b28bfdf
tharanga270 <developerwordpress270@gmail.com>
2024-01-21 13:24:15 +0530

    8.React Redux Toolkit setup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,25 +1,31 @@
-import React from "react";
-import "../styles/RegiserStyles.css";
-import { Form, Input, message } from "antd";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import React from 'react';
+import '../styles/RegiserStyles.css';
+import { Form, Input, message } from 'antd';
+import { useDispatch } from 'react-redux';
+import { showLoading, hideLoading } from '../redux/features/alertSlice';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   //form handler
   const onfinishHandler = async (values) => {
     try {
-      const res = await axios.post("/api/v1/user/login", values);
+      dispatch(showLoading());
+      const res = await axios.post('/api/v1/user/login', values);
+      dispatch(hideLoading());
       if (res.data.success) {
-        localStorage.setItem("token", res.data.token);
-        message.success("Login Successfully");
-        navigate("/");
+        localStorage.setItem('token', res.data.token);
+        message.success('Login Successfully');
+        navigate('/');
       } else {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
-      message.error("something went wrong");
+      message.error('something went wrong');
     }
   };
   return (
