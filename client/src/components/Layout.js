1ccf49409d578f957b6ea7a3e4d3f51ad6cee4eb
tharanga270 <developerwordpress270@gmail.com>
2024-01-22 13:28:13 +0530

    11.Getting User Details

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import '../styles/LayoutStyles.css';
-import { SidebarMenu } from './../Data/data';
-import { Link, useLocation } from 'react-router-dom';
+import { adminMenu, userMenu } from './../Data/data';
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { message } from "antd";
+
 const Layout = ({ children }) => {
-    const location = useLocation();
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // logout funtion
+  const handleLogout = () => {
+    localStorage.clear();
+    message.success("Logout Successfully");
+    navigate("/login");
+  };
+
+  // redering menu list
+  const SidebarMenu = user?.isAdmin ? adminMenu : userMenu;
   return (
     <>
       <div className="main">
@@ -14,21 +30,30 @@ const Layout = ({ children }) => {
               <hr />
             </div>
             <div className="menu">
-              {SidebarMenu.map((menu,index) => {
+              {SidebarMenu.map((menu, index) => {
                 const isActive = location.pathname === menu.path;
                 return (
                   <div key={index}>
-                    <div className={`menu-item ${isActive && "active"}`}>
+                    <div className={`menu-item ${isActive && 'active'}`}>
                       <i className={menu.icon}></i>
                       <Link to={menu.path}>{menu.name}</Link>
                     </div>
                   </div>
                 );
               })}
+              <div className={`menu-item `} onClick={handleLogout}>
+                <i className="fa-solid fa-right-from-bracket"></i>
+                <Link to="/login">Logout</Link>
+              </div>
             </div>
           </div>
           <div className="content">
-            <div className="header">Header</div>
+            <div className="header">
+            <div className="header-content">
+                <i className="fa-solid fa-bell"></i>
+                <Link to="/profile">{user?.name}</Link>
+              </div>
+            </div>
             <div className="body">{children}</div>
           </div>
         </div>
