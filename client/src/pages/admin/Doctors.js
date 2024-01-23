3b2ded3fa8b726f6937479240aa615fa85ce3477
tharanga270 <developerwordpress270@gmail.com>
2024-01-23 16:41:52 +0530

    14.Admin Control Pages

---
+++ client/src/pages/admin/Doctors.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './../../components/Layout';
+
+const Doctors = () => {
+  return (
+    <Layout>
+      <h1>All Doctors</h1>
+      
+    </Layout>
+  );
+};
+
+export default Doctors;
